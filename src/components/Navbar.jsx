46438d528abc435ebee029a6bb42075127400636
codenonsense <114344323+codenonsense@users.noreply.github.com>
2023-11-22 19:34:37 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import {  menu, close } from "../assets";
-import { logo } from "../assets/Ramin.png"
+import { logo } from "../assets/Ramin.png";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
