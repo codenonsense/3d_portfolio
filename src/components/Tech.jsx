92ecc2765fc6fbe508f6e28a85cd135b501527f6
codenonsense <114344323+codenonsense@users.noreply.github.com>
2024-08-19 18:58:03 +0600

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,6 +3,7 @@ import { SectionWrapper } from "./hoc"
 import { technologies } from "../constants"
 
 const Tech = () => {
+  // showing the techonolgies in a circular shape
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
